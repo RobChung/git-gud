a187ea4484dc7a9bc2a83e1a433c009b0ef169ae
KAUR7146 <60115661+KAUR7146@users.noreply.github.com>
2020-01-23 13:26:49 -0500

    Update CardTrick.java

--- src/pkg2020w_ice1_4365/CardTrick.java
+++ src/pkg2020w_ice1_4365/CardTrick.java
@@ -1,25 +1,56 @@
 package pkg2020w_ice1_4365;
-
+import java.util.Scanner;
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects and then asks the user to pick a card and
  * searches the array of cards for the match to the user's card. To be used as starting code in ICE 1
  *
  * @author dancye
  * @author Paul Bonenfant
+ * @modifier AMRITPREET KAUR
+ * Student Id:-991551280
  */
 public class CardTrick {
 
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
         
         Card[] magicHand = new Card[7];
 
         for (int i = 0; i < magicHand.length; i++) {
             Card card = new Card();
-            //card.setValue(insert call to random number generator here)
-            //card.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            card.setValue((int)(Math.random()*13+1));
+            card.setSuit(Card.SUITS[(int)(Math.random()*13+1)]);
+            magicHand[i]=card;
         }
+System.out.println("pick a card");
+System.out.println("enter the face value for card");
+int value=sc.nextInt();
+System.out.println("enter the suit of your card");
+int suitInt=sc.nextInt();
 
-        //insert code to ask the user for Card value and suit, create their card
+Card card=new Card();
+card.setValue(value);
+card.setSuit(Card.SUITS[suitInt-1]);
+
+Card luckynumber=new Card();
+card.setValue(10);
+card.setSuit(Card.SUITS[2]);
+
+
+boolean found=false;
+for(Card magicCard:magicHand)
+{if(magicCard.getValue()==card.getValue() && magicCard.getSuit().equals(card.getSuit()))
+    found=true;
+break;
+    }
+if(found)
+{
+    System.out.println("congrats it is matched");
+}
+else
+{
+    System.out.println("doesnot match");
+}
         // and search magicHand here
         //Then report the result here
     }
